c7f4ce904d60dbce49272b78b2c3234050ac60c5
Narendra Amurti Wijaya <cuuurutterbang@gmail.com>
2024-11-07 11:36:12 +0700

    PHP

---
+++ Kelas/2024-11-05/pages/sejarah.php
@@ -0,0 +1,16 @@
+<?php
+$sql = "SELECT * FROM sejarah";
+
+
+$hasil = mysqli_query($koneksi, $sql);
+while ($row = mysqli_fetch_array($hasil)) {
+?>
+<div class="sejarah">
+    <h2><?= $row [1] ?></h2>
+   <p><?= $row [2] ?></p>
+<?php
+}
+?>
+
+
+
