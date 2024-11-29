19fb7bfceae537f13762cb1a7a3d6538ea53b69b
Narendra Amurti Wijaya <cuuurutterbang@gmail.com>
2024-11-29 23:54:19 +0700

    Tugas Web

---
+++ Kelas/2024-11-08 Tugas Web Sekolah/tugas2.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="atas">
+      <div class="imgjudul">
+      <div class="logo"><img src="./image/cropped-logo-text-kecil-5.png" alt=""></div>
+      <div class="title">"Unggul, Mumpuni, berkarakter"</div>
+      </div>
+    </div>
+
+    <div class="header">
+      <div class="menu">
+        <div class="nav">
+          <ul>
+            <li><a href=""><i class="fas fa-search"></i></a></li>
+            <li><a href="#">Profil Sekolah <i class="fas fa-caret-down" ></i></a>
+              <ul>
+                <li><a href="#" >SEKOLAH "MERPATI"</a></li>
+                <li><a href="#" >Sejarah Sekolah</a></li>
+                <li><a href="#" >Sambutan KepSek</a></li>
+                <li><a href="#" >Visi & Misi</a></li>
+              </ul>
+           </li>
+            <li><a href="#" >Kerja Sama</a></li>
+            <li><a href="#" >Tour Sekolah</a></li>
+            <li><a href="#" >Konsentrasi Keahlian <i class="fas fa-caret-down"></i></a>
+            <ul>
+              <li><a href="#" >RPL | Rekayasa <br> Perangkat Lunak</a></li>
+              <li><a href="#" >DKV | Desain <br> Komunikasi Visual</a></li>
+              <li><a href="#" >LP | Layanan <br>Perbankan </a></li>
+              <li><a href="#" >AK | Akuntansi</a></li>
+              <li><a href="#" >MP | Manajemen <br> Perkantoran</a></li>
+              <li><a href="#" >BD | Bisnis Digital</a></li>
+            </ul>
+            </li>
+            <li><a href="#" >Bidang <i class="fas fa-caret-down"></i></a>
+            <ul>
+              <li><a href="#" >Bursa Kerja Khusus<br>(BKK)</a></li>
+              <li><a href="#" >LSP P1 SMKN 2 <br> Buduran</a></li>
+              <li><a href="#" >Technopark</a></li>
+            </ul>
+            </li>
+            <li><a href="#" >Fitur-fitur <i class="fas fa-caret-down"></i></a>
+            <ul>
+              <li><a href="#" >Karya Siswa</a></li>
+              <li><a href="#" >Informasi</a></li>
+              <li><a href="#" >Galeri</a></li>
+              <li><a href="#" >Artikel</a></li>
+              <li><a href="#" >E-Learning</a></li>
+            </ul>
+            </li>
+            <li><a href="#" >Administrasi <i class="fas fa-caret-down"></i></a>
+            <ul>
+              <li><a href="#" >DAPODIK</a></li>
+              <li><a href="#" >E-Raport</a></li>
+              <li><a href="#" >EDS-PMP</a></li>
+            </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="main">
+    <div class="content">
+      <div class="kiri"></div>
+      <div class="tengah"></div>
+      <div class="kanan"></div>
+    </div>
+
+    <div class="footer">
+      <div class="background"></div>
+    </div>
+
+  <div class="footer-2">
+    <div class="konten">
+    <div class="left">
+      <div class="content-left">
+      <div class="gambar"><img src="./image/cropped-logo-text-kecil-5.png" alt=""></div>
+      <div class="isi">
+        <div class="lokasi-sekolah">
+          <h4>SMKN 2 Buduran</h4>
+          <p>Jl. Jenggolo No.2A Siwalanpanji  Kec. Buduran</p>
+          <p>Kabupaten Sidoarjo, Jawa Timur 61219</p>
+        </div>
+      <br>
+      
+      <div class="data-sekolah">
+        <p>NPSN : 20501695</p>
+        <p>Email : info@smkn2buduran.sch.id</p>
+        <p>No. Telp : (031) 8964034</p>
+      </div>
+    </div>
+      </div>
+    </div>
+
+    <div class="center">
+      <div class="content-center">
+          <div class="isi-center">
+            <p>POSTINGAN KAMI</p>
+            <h6>Kerjasama PKL dengan BDR (Bank Dana Raya) <br>& CV Rizky Barokah </h6>
+            <p>OCTOBER 8, 2024</p>
+
+            <br>
+
+            <h6>Pembinaan GTK SMKN 2 Buduran Bersama Bpk. <br> Dr. Kiswanto, S.Pd, M.Pd</h6>
+            <p>OCTOBER 1, 2024</p>
+
+            <br>
+
+            <h6>Hari Kesaktian Pancasila 1 Oktober 2024</h6>
+            <p>OCTOBER 1, 2024</p>
+          </div>
+      </div>
+    </div>
+    <div class="right">
+      <div class="content-right">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2818.360316930207!2d112.72265385812881!3d-7.4373443591115835!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7e77ba09185a9%3A0x80a96e7778bba87d!2sState%20Professional%20School%202%20Buduran%20Sidoarjo!5e0!3m2!1sen!2sus!4v1732205063072!5m2!1sen!2sus" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+    </div>
+   
+  </div>
+
+  <div class="footer-3">
+    <div class="content-footer-3">
+      <p>Beranda</p>
+    </div>
+  </div>
+
+  <div class="footer-4">
+    <div class="content-footer-4">
+      <img src="./image/cropped-logo-text-kecil-5.png" alt="">
+      <div class="isi-copyright">
+      <p>SMKN 2 Buduran Sidoarjo Copyright © 2024. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+  
+  </div>
+</body>
+</html>
